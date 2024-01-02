dce20d99d88c32b96571f4a405d5f8975d8dac2d
Pratham <prathamg2003@gmail.com>
2024-01-02 22:40:49 +0530

    ui improved

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import { useState } from 'react';
 
-import './App.css'
-
+import './App.css';
 
 function App() {
-
-
   const [count, setCount] = useState(0);
 
   function decreaseHandler() {
@@ -21,14 +18,11 @@ function App() {
   }
 
   return (
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#2c3e50] flex-col gap-10">
+      <div className="text-[#3498db] font-medium text-2xl">Increment & Decrement</div>
 
-    <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
-
-      <div className="text-[#0398d4] font-medium text-2xl">Increment & Decrement</div>
-
-      <div className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]">
-
-        <button onClick={decreaseHandler} className='border-r-2 text-xenter w-20 border-[#bfbfbf] text-5xl'>
+      <div className="bg-[#ecf0f1] flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#2c3e50]">
+        <button onClick={decreaseHandler} className='border-r-2 text-xenter w-20 border-[#bdc3c7] text-5xl'>
           -
         </button>
 
@@ -36,18 +30,15 @@ function App() {
           {count}
         </div>
 
-        <button onClick={increaseHandler} className='border-r-2 text-xenter w-20 border-[#bfbfbf] text-5xl'>
+        <button onClick={increaseHandler} className='border-r-2 text-xenter w-20 border-[#bdc3c7] text-5xl'>
           +
         </button>
-
       </div>
 
-      <button onClick={resetHandler} className='text-lg bg-[#0398d4] text-white px-5 py-2 rounded-sm'>
+      <button onClick={resetHandler} className='text-lg bg-[#3498db] text-white px-5 py-2 rounded-sm'>
         Reset
       </button>
-
     </div>
-
   );
 }
 
